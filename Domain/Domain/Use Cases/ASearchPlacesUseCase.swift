4710218dff8c18a93be07f5319085bd58bd0144a
Daniel Hu <hdmdhr@gmail.com>
2021-11-22 15:48:19 -0700

    Add default `SearchPlacesUseCase`

--- Domain/Domain/Use Cases/ASearchPlacesUseCase.swift
+++ Domain/Domain/Use Cases/ASearchPlacesUseCase.swift
@@ -12,3 +12,19 @@ public protocol ASearchPlacesUseCase {
     func search(query: Places.SearchQuery) async throws -> InnerPagingEnvelop<Places.Place>
     
 }
+
+
+public class SearchPlacesUseCase: ASearchPlacesUseCase {
+    
+    public init(repo: APlacesRepo) {
+        self.repo = repo
+    }
+    
+    
+    private let repo: APlacesRepo
+    
+    public func search(query: Places.SearchQuery) async throws -> InnerPagingEnvelop<Places.Place> {
+        try await repo.search(query: query)
+    }
+    
+}
