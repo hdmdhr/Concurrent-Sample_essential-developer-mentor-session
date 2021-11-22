09e93f3920734b6f113d0ff5a8102d92625c401c
Daniel Hu <hdmdhr@gmail.com>
2021-11-22 16:56:10 -0700

    Add UIViewController+Alert ext to Core

---
+++ Core/Core/UIViewController+Alert.swift
@@ -0,0 +1,29 @@
+//
+//  UIViewController+Alert.swift
+//  Core
+//
+//  Created by 胡洞明 on 2021-11-22.
+//
+
+import UIKit
+
+public extension UIViewController {
+    
+    @discardableResult
+    func presentAlert(title: String?,
+                      message: String?,
+                      style: UIAlertController.Style = .alert,
+                      actions: [UIAlertAction] = [.init(title: "Cancel", style: .cancel)]) -> UIAlertController
+    {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: style)
+        
+        for action in actions {
+            alert.addAction(action)
+        }
+        
+        present(alert, animated: true)
+        
+        return alert
+    }
+    
+}
